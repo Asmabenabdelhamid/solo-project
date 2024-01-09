da6ec691aa9ca40539cc4a0d6ea3db72a892a34a
unknown <asmabenabdelhamid@gmail.com>
2024-01-09 17:01:53 +0100

    done

---
+++ artist.js
@@ -0,0 +1,49 @@
+$('#')
+
+
+
+
+$("#boutton")
+$("#boutton-submit").on("click",function(search))
+
+
+
+
+
+
+
+var counter = 0 
+ function Peintimage ( "article1"){
+    if (counter===article1) {
+        
+        
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+$("#shop")
+
+
+
+
+
+
+
+var articles = {
+
+    reférence : "article1"
+    name :     ;
+    with :      , hi   ;
+    price :   $ ;
+}
+    reférence : "article2"
+   
